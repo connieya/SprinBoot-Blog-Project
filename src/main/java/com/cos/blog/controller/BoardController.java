9a9bf41abcf7f6d2306ebc43cc83b69c03526d7b
gunny6026 <gunny6026@naver.com>
2020-12-26 18:15:35 +0900

    main UI board list

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,22 +1,34 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	BoardService  boardService;
+	
 	// 메인화면 갈때는 인증이 필요 없기 때문에 주석처리
 	//@AuthenticationPrincipal PrincipalDetail principal
-	@GetMapping("/index")
-	public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
+	@GetMapping({"", "/"})
+	public String index(Model model) { // 컨트롤러에서 세션을 어떻게 찾는지?
 		
+		//model은 jsp로 치면 request 정보라고 보면 된다.
+		model.addAttribute("boards", boardService.글목록());
+		//위에 url으로 Model을 통한 데이터 boards가 넘어간다.
 		//WEB-INF/views/index.jsp
 	//	System.out.println("로그인 사용자 아이디 :" +principal.getUsername());
-		return "index";
+		return "index"; //@Controller로 인해 return 할 때 viewResolver가 작동!!
+		//viewResolver는 application.yml에서 지정한
+		//mvc:view:prefix: /WEB-INF/views/suffix: .jsp
+		//의 역할을 해줌
 	}
 	
 	//USER 권한이 필요
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -4,19 +4,15 @@
 <%@ include file="layout/header.jsp"%>
 
 <div class="container">
+<c:forEach var="board" items="${boards}">
 	<div class="card m-2">
 		<div class="card-body">
-			<h4 class="card-title">제목</h4>
+			<h4 class="card-title">${board.title}</h4>
 			<a href="#" class="btn btn-primary">상세보기</a>
 		</div>
 	</div>
-	<div class="card m-2">
-		<div class="card-body">
-			<h4 class="card-title">제목</h4>
-			<p class="card-text">내용</p>
-			<a href="#" class="btn btn-primary">상세보기</a>
-		</div>
-	</div>
+	
+	</c:forEach>
 </div>
 <br>
 
